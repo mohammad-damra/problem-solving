ee5b44226d2039a70edfe76ec062ba1ed19635e5
mohammad-damra <mohammad.s.damra@gmail.com>
2020-12-27 04:04:15 +0200

    slove q15 + q16

--- Q15/main.js
+++ Q15/main.js
@@ -8,8 +8,21 @@ Your function should return the common characters in the same order that they ap
 Do not return duplicate characters and ignore whitespace in your returned string.
 */
 
-function commonCharacters() {
-  // YOUR CODE HERE
+function commonCharacters(str1, str2) {
+	// YOUR CODE HERE
+	let commonChar = '';
+	for (let i = 0; i < str1.length; i++)
+		for (let j = 0; j < str2.length; j++) {
+			if (str1[i] === str2[j])
+				if (str1[i] === ' ') {
+					break;
+				} else {
+					commonChar = commonChar + str1[i];
+				}
+		}
+	commonChar = commonChar.split('');
+	commonChar = commonChar.filter((item, i, commonChar) => commonChar.indexOf(item) === i);
+	return commonChar.join('');
 }
 
 /* 
--- Q16/main.js
+++ Q16/main.js
@@ -6,8 +6,9 @@ console.log('Problem Solving Q:16 ');
 Find the first item that occurs an even number of times in an array.
 */
 
-function evenOccurrence() {
-  // YOUR CODE HERE
+function evenOccurrence(arr) {
+	// YOUR CODE HERE
+	for (let i = 0; i < arr.length; i++) if (arr[i] === arr[i + 1]) return arr[i];
 }
 
 /* 
