6c4246a805fe7ff3c1ed9fe764e9adfddafadae3
mohammad-damra <mohammad.s.damra@gmail.com>
2021-01-13 09:12:25 +0200

    Q20 solved

--- Q20/main.js
+++ Q20/main.js
@@ -6,8 +6,12 @@ console.log('Problem Solving Q: 20');
 Write a function that accepts an array of integers and returns the largest product possible from three of those numbers.
 */
 
-function largestProductOfThree() {
-  // YOUR CODE HERE
+function largestProductOfThree(arr) {
+	// YOUR CODE HERE
+	arr.sort((a, b) => {
+		return a - b;
+	});
+	return arr[arr.length - 3] * arr[arr.length - 2] * arr[arr.length - 1];
 }
 
 /* 
