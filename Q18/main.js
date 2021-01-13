b17e733e5e904b78600455ccfa8942c143f9cdf1
mohammad-damra <mohammad.s.damra@gmail.com>
2021-01-13 08:23:26 +0200

    Q18 solved

--- Q18/main.js
+++ Q18/main.js
@@ -7,8 +7,36 @@ Given an integer, n, return n! (n factorial)
 5! = 5 * 4 * 3 * 2 * 1
 */
 
-function factorial() {
-  // YOUR CODE HERE
+function factorial(num) {
+	// YOUR CODE HERE
+
+	//  First Solution : Recursion
+	/*
+	if (num < 0) return 'must be a positive number';
+	else if (num === 0 || num === 1) return 1;
+  else return num * factorial(num - 1);
+  */
+
+	// Second Solution : While Loop
+	/*
+	let result = num;
+	if (num < 0) return 'must be a positive number';
+	else if (num === 0 || num === 1) return 1;
+	while (num > 1) {
+		num--;
+		result *= num;
+	}
+  return result;
+  */
+
+	// Third Solution : For Loop
+
+	if (num < 0) return 'must be a positive number';
+	else if (num === 0 || num === 1) return 1;
+	for (let i = num - 1; i >= 1; i--) {
+		num *= i;
+	}
+	return num;
 }
 
 /* 
