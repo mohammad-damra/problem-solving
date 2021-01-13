9cbf4c5e3157513d0a263ee080455e107484e878
mohammad-damra <mohammad.s.damra@gmail.com>
2021-01-13 07:59:26 +0200

    Q17 solved

--- Q17/main.js
+++ Q17/main.js
@@ -6,8 +6,15 @@ console.log('Problem Solving Q:17 ');
 Given an arbitrary input string, return the first non-repeating character. For strings with all repeats, return 'sorry'
 */
 
-function firstNonRepeatedCharacter() {
-  // YOUR CODE HERE
+function firstNonRepeatedCharacter(str) {
+	// YOUR CODE HERE
+	for (let i = 0; i < str.length; i++) {
+		let char = str[i];
+		if (str.indexOf(char) == i && str.indexOf(char, i + 1) == -1) {
+			return char;
+		}
+	}
+	return 'none';
 }
 
 /* 
