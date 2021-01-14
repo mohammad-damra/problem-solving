eb8381c1007c2465d39a5294736875b46cf4a3c6
mohammad-damra <mohammad.s.damra@gmail.com>
2021-01-14 21:18:21 +0200

    Q22 solved/ correct but not like the requirements

--- Q22/main.js
+++ Q22/main.js
@@ -3,13 +3,24 @@ console.log('Problem Solving Q: 22');
 /* characterFrequency */
 
 /*  
- Write a function that takes as its input a string and returns an array of 
+ Write a function that takes as its input a str and returns an array of 
  arrays as shown below sorted in descending order by frequency and then by 
  ascending order by character.
 */
 
-function characterFrequency() {
-  // YOUR CODE HERE
+function characterFrequency(str) {
+	// YOUR CODE HERE
+	let freq = [];
+	for (let i = 0; i < str.length; i++) {
+		let character = str.charAt(i);
+		if (freq[character]) {
+			freq[character]++;
+		} else {
+			freq[character] = 1;
+		}
+	}
+
+	return freq;
 }
 
 /* 
