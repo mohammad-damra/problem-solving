3a4f1b2704bfd3236c98344a021f54bcf6313585
mohammad-damra <mohammad.s.damra@gmail.com>
2021-01-14 21:25:53 +0200

    Q25 solved

--- Q25/main.js
+++ Q25/main.js
@@ -6,8 +6,9 @@ console.log('Problem Solving Q: 25');
 Write a JavaScript function to check whether an `input` is an array or not.
 */
 
-function is_array() {
-  // YOUR CODE HERE
+function is_array(arr) {
+	// YOUR CODE HERE
+	return Array.isArray(arr);
 }
 
 /* 
