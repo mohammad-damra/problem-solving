d268341c827885618338446488b7a1d41666d2ee
mohammad-damra <mohammad.s.damra@gmail.com>
2021-01-21 16:02:17 +0200

    Q5 data structures solved

--- 02_Data Structures + Algorithms [27 Q]/exercises/05_fizzbuzz/index.js
+++ 02_Data Structures + Algorithms [27 Q]/exercises/05_fizzbuzz/index.js
@@ -12,6 +12,20 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+	for (let i = 1; i <= n; i++) {
+		if (i % 3 === 0 && i % 5 === 0) {
+			// we also can use if(i % 15 === 0)
+			console.log('fizzbuzz');
+		} else if (i % 3 === 0) {
+			console.log('fizz');
+		} else if (i % 5 === 0) {
+			console.log('buzz');
+		} else {
+			console.log(i);
+		}
+	}
+	return 0;
+}
 
 module.exports = fizzBuzz;
