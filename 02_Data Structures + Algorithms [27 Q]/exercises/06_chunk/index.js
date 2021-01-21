18705cd0fbdce2ec0da86b712683d46a2f25f407
mohammad-damra <mohammad.s.damra@gmail.com>
2021-01-21 17:17:55 +0200

    Q6 data structures solved

--- 02_Data Structures + Algorithms [27 Q]/exercises/06_chunk/index.js
+++ 02_Data Structures + Algorithms [27 Q]/exercises/06_chunk/index.js
@@ -8,6 +8,19 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+	function chunk(array, size) {
+		let chunkArr = [];
+		for (let i = 0; i < array.length; i++) {
+			let last = chunkArr[chunkArr.length - 1];
+			if (!last || last.length === size) {
+				chunkArr.push([ array[i] ]);
+			} else {
+				last.push(array[i]);
+			}
+		}
+		return chunkArr;
+	}
+}
 
 module.exports = chunk;
