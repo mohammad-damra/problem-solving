15e3c537ba97b5004a2754622b44cd0cbbfdf526
mohammad-damra <mohammad.s.damra@gmail.com>
2021-01-21 14:23:46 +0200

    Q1 data structures solved

--- 02_Data Structures + Algorithms [27 Q]/exercises/01_reversestring/index.js
+++ 02_Data Structures + Algorithms [27 Q]/exercises/01_reversestring/index.js
@@ -7,7 +7,15 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-
+	let stack = [];
+	for (let i = 0; i < str.length; i++) {
+		stack.push(str[i]);
+	}
+	let revStr = '';
+	while (stack.length > 0) {
+		revStr += stack.pop();
+	}
+	return revStr;
 }
 
 module.exports = reverse;
