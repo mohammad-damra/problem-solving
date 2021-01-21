34382b8379a3ca23b44827f71b5d82c473abc9a3
mohammad-damra <mohammad.s.damra@gmail.com>
2021-01-21 15:28:37 +0200

    Q3 data structures solved

--- 02_Data Structures + Algorithms [27 Q]/exercises/03_reverseint/index.js
+++ 02_Data Structures + Algorithms [27 Q]/exercises/03_reverseint/index.js
@@ -8,6 +8,28 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+	let str = n + '';
+	let stack = [];
+	for (let i = 0; i < str.length; i++) {
+		stack.push(str[i]);
+	}
+	let revStr = '';
+	while (stack.length > 0) {
+		console.log('stack: ', stack);
+		if (stack[stack.length - 1] === '0') {
+			stack.pop();
+		} else {
+			revStr += stack.pop();
+		}
+	}
+	if (revStr.charAt(revStr.length - 1) === '-') {
+		1;
+		revStr = revStr.substring(0, revStr.length - 1);
+		revStr = '-' + revStr;
+	}
+	revStr *= 1;
+	return revStr;
+}
 
 module.exports = reverseInt;
