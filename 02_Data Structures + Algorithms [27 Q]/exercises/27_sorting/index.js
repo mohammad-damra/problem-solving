067d2f72e2a2e76658929e8a6a603816a91b0253
mohammad-damra <mohammad.s.damra@gmail.com>
2021-01-28 18:41:43 +0200

    solved it

--- 02_Data Structures + Algorithms [27 Q]/exercises/27_sorting/index.js
+++ 02_Data Structures + Algorithms [27 Q]/exercises/27_sorting/index.js
@@ -2,19 +2,59 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = 0; j < arr.length; j++) {
+			if (arr[j] > arr[j + 1]) {
+				let tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+	}
+	return arr;
 }
 
 function selectionSort(arr) {
-
+	for (let i = 0; i < arr.length; i++) {
+		let min = i;
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[j] < arr[min]) {
+				min = j;
+			}
+		}
+		if (min != i) {
+			let tmp = arr[i];
+			arr[i] = arr[min];
+			arr[min] = tmp;
+		}
+	}
+	return arr;
 }
 
 function mergeSort(arr) {
-
+	if (arr.length <= 1) {
+		return arr;
+	}
+	const middle = Math.floor(arr.length / 2);
+	const left = arr.slice(0, middle);
+	const right = arr.slice(middle);
+	return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
-
+	let resultArray = [],
+		leftIndex = 0,
+		rightIndex = 0;
+	while (leftIndex < left.length && rightIndex < right.length) {
+		if (left[leftIndex] < right[rightIndex]) {
+			resultArray.push(left[leftIndex]);
+			leftIndex++;
+		} else {
+			resultArray.push(right[rightIndex]);
+			rightIndex++;
+		}
+	}
+	return resultArray.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
