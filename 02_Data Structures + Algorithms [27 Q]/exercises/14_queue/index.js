6d8c154863e36da7ad74b1e303d7bf94b41fe011
mohammad-damra <mohammad.s.damra@gmail.com>
2021-01-28 16:50:18 +0200

    solved it

--- 02_Data Structures + Algorithms [27 Q]/exercises/14_queue/index.js
+++ 02_Data Structures + Algorithms [27 Q]/exercises/14_queue/index.js
@@ -11,24 +11,12 @@
 class Queue {
 	constructor() {
 		this.data = [];
-		this.rear = 0;
-		this.size = 10;
 	}
-
-	enqueue(element) {
-		this.data[this.rear] = element;
-		this.rear = this.rear + 1;
+	add(value) {
+		this.data.unshift(value);
 	}
-
-	enqueue(element) {
-		if (this.rear < this.size) {
-			this.data[this.rear] = element;
-			this.rear = this.rear + 1;
-		}
-	}
-
-	length() {
-		return this.rear;
+	remove() {
+		return this.data.pop();
 	}
 }
 
