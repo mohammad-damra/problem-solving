b3d219aad1227ca40453e1c137a1e8274db1a216
mohammad-damra <mohammad.s.damra@gmail.com>
2021-01-28 17:37:12 +0200

    solved it

--- 02_Data Structures + Algorithms [27 Q]/exercises/19_midpoint/index.js
+++ 02_Data Structures + Algorithms [27 Q]/exercises/19_midpoint/index.js
@@ -12,6 +12,15 @@
 //   l.insertLast('c')
 //   midpoint(l); // returns { data: 'b' }
 
-function midpoint(list) {}
+function midpoint(list) {
+	let node1 = list.head;
+	let node2 = list.head;
+	while (node2.next && node2.next.next) {
+		node1 = node1.next;
+		node2 = node2.next.next;
+	}
+
+	return node1;
+}
 
 module.exports = midpoint;
