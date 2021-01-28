d8049233ec223f63beda1e84d0453a7f05027ba3
mohammad-damra <mohammad.s.damra@gmail.com>
2021-01-28 17:28:10 +0200

    not completed

--- 02_Data Structures + Algorithms [27 Q]/exercises/18_linkedlist/index.js
+++ 02_Data Structures + Algorithms [27 Q]/exercises/18_linkedlist/index.js
@@ -2,8 +2,41 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+	constructor(data) {
+		this.data = data;
+		this.next = null;
+	}
+}
 
-class LinkedList {}
+class LinkedList {
+	constructor(head = null) {
+		this.head = head;
+	}
+	size() {
+		let count = 0;
+		let node = this.head;
+		while (node) {
+			count++;
+			node = node.next;
+		}
+		return count;
+	}
+	clear() {
+		this.head = null;
+	}
+	getFirst() {
+		return this.head;
+	}
+	getLast() {
+		let lastNode = this.head;
+		if (lastNode) {
+			while (lastNode.next) {
+				lastNode = lastNode.next;
+			}
+		}
+		return lastNode;
+	}
+}
 
 module.exports = { Node, LinkedList };
